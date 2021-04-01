35280c0324f531974f84bc719adfb89256ffaa91
Alexey Debelov <alienjustmail@gmail.com>
2021-04-01 15:31:22 +0500

    Added methods to open file*s* dialog Also updated version

--- IWindowSystem.cs
+++ IWindowSystem.cs
@@ -3,6 +3,7 @@
     public interface IWindowSystem
     {
         string ShowOpenFileDialog(string dialogTitle, string filter);
+        string[] ShowOpenFilesDialog(string dialogTitle, string filter);
         string ShowSaveFileDialog(string dialogTitle, string filter);
         void ShowMessageBox(string message, string caption);
         BinaryChoise ShowYesNoDialog(string message, string caption);
--- IWindowSystemAsync.cs
+++ IWindowSystemAsync.cs
@@ -5,11 +5,15 @@ namespace AlienJust.Support.UI.Contracts
     public interface IWindowSystemAsync
     {
         Task<string> ShowOpenFileDialogAsync(string dialogTitle, string filter);
+        
+        Task<string[]> ShowOpenFilesDialogAsync(string dialogTitle, string filter);
 
         Task<string> ShowSaveFileDialogAsync(string dialogTitle, string filter);
 
         Task<BinaryChoise> ShowYesNoDialogAsync(string message, string caption);
 
         Task ShowMessageBoxAsync(string message, string caption);
+
+        Task<string> ShowOpenFolderDialogAsync(string dialogTitle, string defaultDirectory = null);
     }
 }
